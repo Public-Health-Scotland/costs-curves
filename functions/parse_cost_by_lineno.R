1263de3f0e99e8f3fdeb90b4581555178a979c37
maiana-sanjuan <maiana.sanjuan@phs.scot>
2026-03-25 18:09:53 +0000

    feat:add total staff costs

--- functions/parse_cost_by_lineno.R
+++ functions/parse_cost_by_lineno.R
@@ -4,6 +4,9 @@ parse_cost_by_lineno <- function(sfr){
   source("functions/recode_sfr.R")
   source("functions/add_patient_type_variable.R")
   
+  # get formname
+  my_formname <- unique(sfr$formname)
+  
   # list of column names to standardise
   rogue_colnames <- c("no_of_discharges", "no_of_cases", "total_attendances", 
                       "no_of_attendances")
@@ -18,15 +21,50 @@ parse_cost_by_lineno <- function(sfr){
   
   # add patient type variable
   costs_by_lineno <- add_patient_type_variable(costs_by_lineno)
+  
+  # add a column with total cost for all types of staff
+  if(my_formname == "SFR 5.7N"){
+    
+    costs_by_lineno <- costs_by_lineno |> 
+      
+      # total costs for all types of staff
+      mutate(all_staff = ahp_staff_direct_supplies_number +
+            nursing_staff_number +
+            other_direct_care_number + pharmacy_staff_direct_supplies_number
+            )
+      
+  } else if (my_formname == "SFR 5.9"){
+    costs_by_lineno <- costs_by_lineno |> 
+      
+      # total costs for all types of staff
+      mutate(all_staff = ahp_staff_direct_supplies_number  + 
+               medical_dental_staff_number + nursing_staff_number +
+               other_direct_care_number + pharmacy_staff_direct_supplies_number)
+    
+  } else {
+    
+    costs_by_lineno <- costs_by_lineno |> 
+      
+      # total costs for all types of staff
+      mutate(all_staff = ahp_staff_direct_supplies_number  + 
+               medical_dental_staff_number + nursing_staff_number +
+               other_direct_care_number + pharmacy_staff_direct_supplies_number +
+               theatre_staff_direct_supplies_number)
+    
+  }
 
-  # sum costs by line number and patient type
+ 
+
   costs_by_lineno <- costs_by_lineno |> 
+    # aggregate by patient type and line number
     group_by(ipdc, lineno) |> 
     summarise(across(where(is.numeric), ~sum(.x)), .groups = "drop") |> 
     pivot_longer(where(is.numeric) & !starts_with(standard_colname),
                  names_to = "staff_type", values_to = "cost") |>
-    # setting fixed/variable costs to 100% fixed
+   
+     # setting fixed/variable costs to 100% fixed
     mutate(fixed_costs_prop = 1, variable_costs_prop = 0) |> 
+    
     # calculate fixed and variable costs
     mutate(fixed_cost = cost * fixed_costs_prop, 
            variable_cost = cost * variable_costs_prop)
@@ -34,5 +72,5 @@ parse_cost_by_lineno <- function(sfr){
   return(costs_by_lineno)
 }
 
-# test <- parse_cost_by_lineno(sfr_53)
+# test <- parse_cost_by_lineno(sfr_57n)
 
